c62254f5f7f9dc9a487dc456fbb87a7bbffd5ef6
nthnn <nathanneisip@gmail.com>
2023-11-06 01:43:46 +0800

    Improved virtual machine jargons.

--- ankovm/installer.go
+++ ankovm/installer.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/mattn/anko/env"
+	"github.com/nthnn/AnkoWeb/ankovm/std"
 	"github.com/nthnn/AnkoWeb/logger"
 )
 
@@ -13,10 +14,22 @@ func installDefinition(vmEnv *env.Env, symbol string, value interface{}) {
 	}
 }
 
-func installDefaults(vmEnv *env.Env, path string, w http.ResponseWriter, r *http.Request) {
-	installDefinition(vmEnv, "echo", echoFn(w))
-	installDefinition(vmEnv, "include", includeFn(vmEnv, path))
+func installWebFunc(vmEnv *env.Env, path string, w http.ResponseWriter, r *http.Request) {
+	installDefinition(vmEnv, "echo", std.EchoFn(w))
+	installDefinition(vmEnv, "include", std.IncludeFn(vmEnv, path))
 
-	installDefinition(vmEnv, "httpHeaders", httpHeaderFn(r))
-	installDefinition(vmEnv, "httpRemote", httpRemoteFn(r))
+	installDefinition(vmEnv, "httpHeaders", std.HttpHeaderFn(r))
+	installDefinition(vmEnv, "httpRemote", std.HttpRemoteFn(r))
+}
+
+func installJsonFunc(vmEnv *env.Env, path string, w http.ResponseWriter, r *http.Request) {
+	installDefinition(vmEnv, "mapToJson", std.MapToJsonFn)
+	installDefinition(vmEnv, "jsonToMap", std.JsonToMapFn)
+	installDefinition(vmEnv, "jsonPrettify", std.JsonPrettifyFn)
+	installDefinition(vmEnv, "lastJsonError", std.LastJsonErrorFn)
+}
+
+func installDefaults(vmEnv *env.Env, path string, w http.ResponseWriter, r *http.Request) {
+	installWebFunc(vmEnv, path, w, r)
+	installJsonFunc(vmEnv, path, w, r)
 }
--- ankovm/vm.go
+++ ankovm/vm.go
@@ -1,93 +1,17 @@
 package ankovm
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/mattn/anko/env"
 	"github.com/mattn/anko/parser"
 	"github.com/mattn/anko/vm"
 	"github.com/nthnn/AnkoWeb/logger"
+	"github.com/nthnn/AnkoWeb/util"
 )
 
-func parseAwpFile(fileName string, fileContent string) (string, error) {
-	insideAnko := false
-	hasPrevLt := false
-	hasPrevPr := false
-
-	parsed := "echo(\""
-	idx := 0
-
-	for idx < len(fileContent) {
-		current := rune(fileContent[idx])
-
-		if hasPrevLt && current == '%' {
-			if insideAnko {
-				return "", errors.New("already inside Anko context")
-			}
-
-			hasPrevLt = false
-			insideAnko = true
-
-			parsed += "\");"
-			idx += 1
-			continue
-		} else if hasPrevPr && current == '>' {
-			if !insideAnko {
-				return "", errors.New("enclosing Anko outside context")
-			}
-
-			hasPrevPr = false
-			insideAnko = false
-
-			parsed += "echo(\""
-			idx += 1
-			continue
-		} else if (hasPrevLt && current != '%') ||
-			(hasPrevPr && current != '>') {
-			if hasPrevLt {
-				parsed += "<"
-			} else if hasPrevPr {
-				parsed += "%"
-			}
-
-			hasPrevLt = false
-			hasPrevPr = false
-		} else {
-			if current == '<' {
-				hasPrevLt = true
-
-				idx += 1
-				continue
-			} else if current == '%' {
-				hasPrevPr = true
-
-				idx += 1
-				continue
-			}
-		}
-
-		if current == '"' && !insideAnko {
-			parsed += "\\\""
-		} else if current == '\n' && !insideAnko {
-			parsed += "\\n"
-		} else {
-			parsed += string(current)
-		}
-
-		idx += 1
-	}
-
-	if insideAnko {
-		return "", errors.New("unenclosed Anko context, encountered end-of-file")
-	}
-
-	parsed += "\")"
-	return parsed, nil
-}
-
 func RunScript(path string, fileName string, fileContents string, w http.ResponseWriter, r *http.Request) {
-	parsed, err := parseAwpFile(fileName, fileContents)
+	parsed, err := util.ParseAwpFile(fileName, fileContents)
 	if err != nil {
 		logger.Error("Error: " + err.Error())
 		return
